1bd79edd2ee4b329d0ba6042e3abfea1cdf6ca37
BarbaraPFloresRios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-13 20:18:59 +0000

    update main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print(2)
+# imports
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def print_head_data(path):
+    df = pd.read_csv(path)
+    print(df.head(5))
+
+
+if __name__ == "__main__":
+    print_head_data("LinkedInTechJobsDataset.csv")

69ca516338462629576d70a84c4316ddad620b0b
BarbaraPFloresRios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-14 02:20:44 +0000

    changes in main

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def print_head_data(path):
+# Read Dataset and exploring
+def exploring_data(path):
     df = pd.read_csv(path)
-    print(df.head(5))
-
+    print(df.head(5),"/n")
+    print(df.info(),"/n")
+    print(df.describe(),"/n")
 
 if __name__ == "__main__":
-    print_head_data("LinkedInTechJobsDataset.csv")
+    exploring_data("LinkedInTechJobsDataset.csv")

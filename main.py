502e8680ab4863036bef6cab4f10476c71e0f745
BarbaraPFloresRios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-14 03:05:28 +0000

    change in main, add plot

--- main.py
+++ main.py
@@ -9,5 +9,19 @@ def exploring_data(path):
     print(df.info(),"/n")
     print(df.describe(),"/n")
 
+def pie_plot(path, variable):
+    df = pd.read_csv(path)
+    category_count = df[variable].value_counts()
+    plt.figure(figsize=(6, 6))   
+    explode = (0.1, 0, 0, 0)  # Esto separará la primera porción (índice 0) del resto.
+    plt.pie(category_count, labels=category_count.index, autopct='%1.1f%%', startangle=180, explode=explode)
+    plt.title("Number of job positions with respect to the variable {0} ".format(variable))
+    plt.axis('equal')  
+    plt.tight_layout()
+    plt.savefig("{0}.png".format(variable))
+  
+
 if __name__ == "__main__":
     exploring_data("LinkedInTechJobsDataset.csv")
+    pie_plot("LinkedInTechJobsDataset.csv",
+             "Involvement")
